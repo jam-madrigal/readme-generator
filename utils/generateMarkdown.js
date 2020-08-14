8d11fadf01102964cc5afef21b1b75a74bcaf1e2
Alex Madrigal <jam.madrigal@gmail.com>
2020-08-14 15:52:31 -0700

    cleaned up conditionals, updated readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,14 +7,14 @@ function generateMarkdown(data) {
 
   if (data.license === "Eclipse Public License 1.0 (EPL-1.0)") {
 
-      badge = `[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`;
+      badge = `![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`;
       licenseInfo = `This project is licensed under Eclipse Public License 1.0. Read more at https://opensource.org/licenses/EPL-1.0`;
 
   }
 
   else if (data.license === "CC0 1.0 Universal (CC0 1.0)Public Domain Dedication") {
 
-    badge = '[![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)]'
+    badge = '![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)'
     licenseInfo = `This project is licensed under CC0 1.0 Universal (CC0 1.0)
     Public Domain Dedication. Read more at https://creativecommons.org/publicdomain/zero/1.0/`
 
@@ -22,7 +22,7 @@ function generateMarkdown(data) {
 
   else {
 
-    badge = `[![License: CC BY-NC-ND 4.0](https://licensebuttons.net/l/by-nc-nd/4.0/80x15.png)]`
+    badge = `![License: CC BY-NC-ND 4.0](https://licensebuttons.net/l/by-nc-nd/4.0/80x15.png)`
     licenseInfo = `This project is licensed under Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0). Read more at https://creativecommons.org/licenses/by-nc-nd/4.0/`
 
   }
