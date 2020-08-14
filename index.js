7f1b8026c76da2dcdee77a34c10fd017ecdcbc6f
Alex Madrigal <jam.madrigal@gmail.com>
2020-08-14 12:02:42 -0700

    laying down the basic prompting framework for inquirer function, made questions array

---
+++ index.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+// array of questions for user
+const questions = [
+    "Enter your project title.",
+    "Enter a project description.",
+    "Enter installation information.",
+    "Enter usage information.",
+    "Choose a license for your project.",
+    "List your project contributors",
+    "List any tests for your project.",
+    "Write how to ask questions about the project.",
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+    inquirer
+        .prompt([
+        {
+        type: "input",
+        message: `${questions[1]}`,
+        name: "title"
+        },
+
+        {
+        type: "input",
+        message: `${questions[2]}`,
+        name: "description"
+        },
+
+        {
+        type: "input",
+        message: `${questions[3]}`,
+        name: "installation"
+        },
+
+        {
+        type: "list",
+        message: `${questions[4]}`,
+        name: "usage"
+        choices: 
+            [
+            "Eclipse Public License 1.0 (EPL-1.0)",
+            "CC0 1.0 Universal (CC0 1.0)Public Domain Dedication",
+            "Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)",
+            ]
+            {
+            type: "input",
+            message: `${questions[5]}`,
+            name: "contributors"
+            },
+
+
+        },
+
+
+    ]);
+};
+
+// function call to initialize program
+init();
+
