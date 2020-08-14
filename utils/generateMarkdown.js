109a0b9846e0ba87cc3cc0f2e44e916b634f1451
Alex Madrigal <jam.madrigal@gmail.com>
2020-08-14 15:15:40 -0700

    badge generation is now functioning based on user input

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,24 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  let badge = ``
+  
+  // Declaring a variable for a badge, and setting conditionals that change the badge based on user input
+  let badge = "";
   if (data.license === "Eclipse Public License 1.0 (EPL-1.0)") {
       badge = `[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`
   }
+
+  if (Data.license === "CC0 1.0 Universal (CC0 1.0)Public Domain Dedication") {
+    badge = '[![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)]'
+  }
+
+  else {
+    badge = `[![License: CC BY-NC-ND 4.0](https://licensebuttons.net/l/by-nc-nd/4.0/80x15.png)]`
+  }
+
+// Generating the readme, basically writing out a whole readme in a template literal
+
   return `
-${badge};
+${badge}
 # ${data.title}
 ---
 
