942bc883cb749483e52d37e1fc3f87595b960dd3
Alex Madrigal <jam.madrigal@gmail.com>
2020-08-14 14:45:30 -0700

    readme generation test

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,9 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `
-  # ${data.title}
+# ${data.title}
+---
+##${data.description}
 
 # Table of contents
 - [Title](#${data.title})
